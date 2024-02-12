612e7b1070e773580785cabf6ff31d9a5bbee192
BitBox417 <kjyzx1748@gmail.com>
2024-02-12 13:40:17 +0800

    帳號功能 - 檢查註冊的 email 隱藏返回的密碼

--- springboot-mall/src/main/java/com/trevor/springbootmall/dao/UserDao.java
+++ springboot-mall/src/main/java/com/trevor/springbootmall/dao/UserDao.java
@@ -5,6 +5,9 @@ import com.trevor.springbootmall.model.User;
 
 public interface UserDao {
     User getUserById(Integer userId);
+
+    User getUserByEmail(String email);
+
     Integer createUser(UserRegisterRequest userRegisterRequest);
 
 
